3ae7efdc84db0331a7b6df45a9671b036be1a592
Lucas Lowe <lowel9295@gmail.com>
2024-10-06 15:35:02 -0500

    Add tests for all symbols

--- src/test/java/bsu/edu/cs222/RomanNumeralConverterTest.java
+++ src/test/java/bsu/edu/cs222/RomanNumeralConverterTest.java
@@ -10,4 +10,40 @@ public class RomanNumeralConverterTest {
 
         Assertions.assertEquals("I", rnc.convert(1));
     }
+    @Test
+    public void testConversionOf5() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("V", rnc.convert(5));
+    }
+    @Test
+    public void testConversionOf10() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("X", rnc.convert(10));
+    }
+    @Test
+    public void testConversionOf50() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("L", rnc.convert(50));
+    }
+    @Test
+    public void testConversionOf100() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("C", rnc.convert(100));
+    }
+    @Test
+    public void testConversionOf500() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("D", rnc.convert(500));
+    }
+    @Test
+    public void testConversionOf1000() {
+        RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+        Assertions.assertEquals("M", rnc.convert(1000));
+    }
 }
