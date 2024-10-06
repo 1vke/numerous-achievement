7205eabe0c956d3447821e18b3647505ffc7e0c7
Lucas Lowe <lowel9295@gmail.com>
2024-10-06 15:32:34 -0500

    Update RomanNumeralConverter to support unit test

--- src/main/java/bsu/edu/cs222/RomanNumeralConverter.java
+++ src/main/java/bsu/edu/cs222/RomanNumeralConverter.java
@@ -1,7 +1,22 @@
 package bsu.edu.cs222;
 
+import java.util.HashMap;
+
 public class RomanNumeralConverter {
-    public String convert(int i) {
-        return null;
+    public String convert(int value) {
+        StringBuilder output = new StringBuilder();
+        int number = value;
+
+        String[] romanNumerals = {"M", "D", "C", "L", "X", "V", "I"};
+        int[] romanNumeralValues = {1000, 500, 100, 50, 10, 5, 1};
+
+        for (int i = 0; i < romanNumerals.length; i++) {
+            while (number >= romanNumeralValues[i]) {
+                number -= romanNumeralValues[i];
+                output.append(romanNumerals[i]);
+            }
+        }
+
+        return output.toString();
     }
 }
